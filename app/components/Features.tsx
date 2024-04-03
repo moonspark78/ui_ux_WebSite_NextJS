8e639c329ab44f32edf6b9837389eccd0481583e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-03 17:09:10 +0200

    :art: working on the Features components  :art:

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,6 +3,7 @@ import feature1 from "../../public/assets/feature-1.svg";
 import feature2 from "../../public/assets/feature-2.svg";
 import feature3 from "../../public/assets/feature-3.svg";
 import check from "../../public/assets/check.svg";
+import blueArrow from "../../public/assets/blue-button.svg";
 
 import Image from "next/image";
 
@@ -22,12 +23,27 @@ const Features = () => {
             dashboard.
           </p>
           <ul className="flex flex-col gap-y-3">
-            <li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
               <span>
                 <Image src={check} alt="check"/>
               </span>
+              Lorem ipsum dolor sit amet
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={check} alt="check"/>
+              </span>
+              Consectetur adipiscing elit
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={check} alt="check"/>
+              </span>
+              Sed do eiusmod tempor incididunt ut labore 
             </li>
           </ul>
+
+          <p className="text-[#0085FF] pt-[24px] flex items-center gap-x-4 font-medium">Learn More <span><Image src={blueArrow} alt="arrow"/></span></p>
         </div>
       </div>
     </div>
